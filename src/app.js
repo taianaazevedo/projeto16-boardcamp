48f3ecb3638e7282e2d3a21760feed9ba9e9b435
taianaazevedo <taianazev@gmail.com>
2023-02-08 12:06:21 -0300

    pastas criadas

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+
+
+dotenv.config();
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`Server running in port: ${port}`));
