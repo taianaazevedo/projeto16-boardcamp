c0cd0b0142fd5592265a86da4b1dff28e98f971d
taianaazevedo <taianazev@gmail.com>
2023-02-09 18:17:14 -0300

    fix: cpf validation regex

--- src/schemas/customers.schema.js
+++ src/schemas/customers.schema.js
@@ -3,7 +3,7 @@ import joi from 'joi'
 export const customerSchema = joi.object({
     name: joi.string().required(),
     phone: joi.string().min(10).max(11).required(),
-    cpf: joi.string().length(10).pattern(/^[0-9]+$/).required(),
+    cpf: joi.string().length(11).regex(/^[0-9]+$/).required(),
     birthday: joi.date().iso().required()
 
 })
